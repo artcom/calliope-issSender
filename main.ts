0547447a9e22979c6e99416224c57c706e22346b
elRadish <andreas.rettig@artcom.de>
2022-12-22 15:19:44 +0100

    Add topic

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 radio.setGroup(1)
 input.setAccelerometerRange(AcceleratorRange.EightG)
 basic.forever(function () {
-    radio.sendString("" + (input.acceleration(Dimension.Strength)))
+    radio.sendString("iss/onAcc " + input.acceleration(Dimension.Strength))
+    control.waitMicros(20000)
 })
